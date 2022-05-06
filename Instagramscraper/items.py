3be1c32f20ae28fdd52431cc3069f2586347b2df
Andrey <sebkd@mail.ru>
2022-05-07 02:37:41 +0300

    add auth and follow to parse_user

--- Instagramscraper/items.py
+++ Instagramscraper/items.py
@@ -8,5 +8,9 @@ import scrapy
 
 class InstagramscraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    user_id = scrapy.Field()
+    username = scrapy.Field()
+    photo = scrapy.Field()
+    likes = scrapy.Field()
+    post_data = scrapy.Field()
     pass
--- Instagramscraper/spiders/instaspider.py
+++ Instagramscraper/spiders/instaspider.py
@@ -1,5 +1,9 @@
+import re
+
 import scrapy
 from scrapy.http import HtmlResponse
+
+import setting_instagram
 
 
 class InstaspiderSpider(scrapy.Spider):
@@ -7,5 +11,42 @@ class InstaspiderSpider(scrapy.Spider):
     allowed_domains = ['instagram.com']
     start_urls = ['https://www.instagram.com']
 
+    inst_login_link = 'https://www.instagram.com/accounts/login/ajax/'
+    inst_login = setting_instagram.LOGIN_INSTAGRAM
+    inst_pwd = setting_instagram.PASS_INSTAGRAM
+    inst_parse_user = setting_instagram.INSTAGRAM_PARSE_USER
+
     def parse(self, response: HtmlResponse):
-        pass
+        csrf = self.fetch_csrf_token(response.text)
+        yield scrapy.FormRequest(
+            self.inst_login_link,
+            method='POST',
+            callback=self.login,
+            formdata={
+                'username': self.inst_login,
+                'enc_password': self.inst_pwd,
+            },
+            headers={
+                'X-CSRFToken': csrf,
+            },
+        )
+
+    def login(self, response: HtmlResponse):
+        json_body = response.json()
+        if json_body['authenticated']:
+            # здесь можно сделать цикл из пользователей которых парсим
+            yield response.follow(f'/{self.inst_parse_user}/',
+                                  callback=self.parse_inst_user,
+                                  cb_kwargs={
+                                      'username': self.inst_parse_user
+                                  },
+                                  )
+
+    @staticmethod
+    def fetch_csrf_token(text):
+        """Получение CSRF токена для авторизации из кода HTML страницы"""
+        match = re.search('\"csrf_token\":\"\\w+\"', text).group()
+        return match.split(':').pop().replace(r'"', '')
+
+    def parse_inst_user(self, response: HtmlResponse, username):
+        print()
